93bbf4a3a2b254a25c88a5353910195af967647c
DeveloperNeiser <neyser995p@gmail.com>
2020-07-16 01:12:36 -0500

    Authentication Users

---
+++ WebApi/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "GET,POST", exposedHeaders: "X-My-Header")]
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("api/Authentication/{user}/{password}/")]
+        public HttpResponseMessage Authentication(string user, string password)
+        {
+            try
+            {
+                LoginRepository loginRepository = new LoginRepository();
+                var result = loginRepository.Authentication(user, password);
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    success = true,
+                    result
+                });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Success = false,
+                    Error = e.Message
+                });
+            }
+        }
+    }
+}
