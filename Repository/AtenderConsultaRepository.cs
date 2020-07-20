3e7122a560a155bec37672f232a1b5cc8048c325
ANTONY GABRIEL CUMPA ESPINOZA <U19306487@utp.edu.pe>
2020-07-19 23:56:08 -0500

    AtenderConsulta

---
+++ Repository/AtenderConsultaRepository.cs
@@ -0,0 +1,39 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AtenderConsultaRepository
+    {
+        private ConexionBD conexion = new ConexionBD();
+        public string InsertAtenderConsulta(AtenderConsulta atenderConsulta)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion.AbrirConexion();
+
+            comando.CommandText = "[USP_I_RegistrarAtenderConsulta]";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@p_CodMedicamento", atenderConsulta.CodMedicamento);
+            comando.Parameters.AddWithValue("@p_Indicacion", atenderConsulta.Indicacion);
+            comando.Parameters.AddWithValue("@p_FechaAtencion", atenderConsulta.FechaAtencion);
+            comando.Parameters.AddWithValue("@p_CodConsulta", atenderConsulta.CodConsulta);
+            comando.Parameters.AddWithValue("@p_CodDoctor", atenderConsulta.CodDoctor);
+            comando.Parameters.AddWithValue("@p_Examen", atenderConsulta.Examen);
+            comando.Parameters.AddWithValue("@p_Diagnostico", atenderConsulta.Diagnostico);
+            comando.Parameters.AddWithValue("@p_EstadoConsulta", atenderConsulta.EstadoConsulta);
+            comando.ExecuteNonQuery();
+
+
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+
+            return "OK";
+        }
+    }
+}
---
+++ WebApi/Controllers/AtenderConsultaController.cs
@@ -0,0 +1,42 @@
+﻿using Entity;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "GET,POST", exposedHeaders: "X-My-Header")]
+    public class AtenderConsultaController : ApiController
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("api/GuardarAtenderConsulta")]
+        public HttpResponseMessage GuardarMedicamento(AtenderConsulta atenderConsulta)
+        {
+            try
+            {
+                AtenderConsultaRepository atenderConsultaRepository = new AtenderConsultaRepository();
+                var result = atenderConsultaRepository.InsertAtenderConsulta(atenderConsulta);
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    success = true,
+                    result
+                });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Success = false,
+                    Error = e.Message
+                });
+            }
+        }
+
+    }
+}
